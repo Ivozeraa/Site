2428aa97b0663fceabc05380f3fc15980cf7c567
Ivozeraa <joseivo2008@gmail.com>
2024-03-21 21:35:47 -0300

    UP - Sites

---
+++ Tests/Infor-site/navbar.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    var navbarMenu = document.getElementById('navbarMenu');
+    navbarMenu.classList.toggle('active');
+
+    var title1 = document.querySelector('.title-1');
+    if (navbarMenu.classList.contains('active')) {
+        title1.classList.add('navbar-active');
+        title1.style.top = '120px'; 
+    } else {
+        title1.classList.remove('navbar-active');
+        title1.style.top = '0px';
+    }
+}
