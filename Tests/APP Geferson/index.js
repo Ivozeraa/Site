d407b7d7b44483857dc4aa6a3010575f12000f34
Ivozeraa <joseivo2008@gmail.com>
2024-04-11 19:27:59 -0300

    up

--- Tests/APP Geferson/index.js
+++ Tests/APP Geferson/index.js
@@ -2,32 +2,40 @@ window.onload = function(){
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const colorPicker = document.getElementById('colorPicker');
-    const clearbttn = document.getElementById('clearBttn');
+    const clearBttn = document.getElementById('clearBttn');
 
-    let isDrawning = false;
-    let lastX = 0
-    let lastY = 0
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
 
     function draw(e){
-        if(!isDrawning) return;
+        if(!isDrawing) return;
         ctx.strokeStyle = colorPicker.value;
         ctx.beginPath();
         ctx.moveTo(lastX, lastY);
-        ctx.lineTo(e.offsetX, e.offsety);
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
-        [lastX, lastY] = [e.offsetX, e.offsety];
+        lastX = e.offsetX;
+        lastY = e.offsetY;
     }
 
     canvas.addEventListener('mousedown', (e) => {
-        isDrawning = true
-        [lastX, lastY] = [e.offsetX, e.offsetY]
-    })
+        isDrawing = true;
+        lastX = e.offsetX;
+        lastY = e.offsetY;
+    });
 
-    canvas.addEventListener('mousemove', draw)
-    canvas.addEventListener('mouseup', () => isDrawning = false)
-    canvas.addEventListener('mouseout', () => isDrawning = false)
+    canvas.addEventListener('mousemove', draw);
 
-    clearbttn.addEventListener('click', () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-    })
-}
+    canvas.addEventListener('mouseup', () => {
+        isDrawing = false;
+    });
+
+    canvas.addEventListener('mouseout', () => {
+        isDrawing = false;
+    });
+
+    clearBttn.addEventListener('click', () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+};
